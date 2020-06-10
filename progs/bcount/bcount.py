ee7161218d8247ce7645a0453594074bceaa91e0
Code Master Neel <41355538+codemstrneel@users.noreply.github.com>
2020-06-10 14:13:48 -0700

    Update bcount.py

--- progs/bcount/bcount.py
+++ progs/bcount/bcount.py
@@ -1,9 +1,3 @@
-"""
-ID: neelkolhe
-TASK: bcount
-"""
-
-
 inpFile=open('bcount.in','r')
 outFile=open('bcount.out','w')
 
@@ -13,6 +7,7 @@ numberCows=int(arr[0])
 
 numberRanges=int(arr[1])
 allCows=[0]
+newArr=[[0,0,0]]
 cow1=0
 cow2=0
 cow3=0
@@ -24,44 +19,20 @@ for i in range (numberCows):
     cow2+=1
   elif allCows[i+1]==3:
     cow3+=1
+  newArr.append([cow1,cow2,cow3])
+#print(newArr)
 
-print(allCows)
-print(cow1)
-print(cow2)
-print(cow3)
-ranges=[]
+allQuer=[]
 for i in range(numberRanges):
-  tempArr=inpFile.readline().split()
-  ranges.append([int(tempArr[0]),int(tempArr[1])])
+  x=input().split()
+  allQuer.append([int(x[0]),int(x[1])])
 
-for i in range (numberRanges):
-  TempCounter1=0
-  TempCounter2=0
-  TempCounter3=0
-  if (ranges[i][1]-ranges[i][0]+1)>(numberCows/2):
-    for j in range (1,ranges[i][0]):
-      if allCows[j]==1:
-        TempCounter1+=1
-      elif allCows[j]==2:
-        TempCounter2+=1
-      elif allCows[j]==3:
-        TempCounter3+=1
-    for j in range (ranges[i][1]+1,numberCows+1):
-      if allCows[j]==1:
-        TempCounter1+=1
-      elif allCows[j]==2:
-        TempCounter2+=1
-      elif allCows[j]==3:
-        TempCounter3+=1
-    outFile.write(str(cow1-TempCounter1)+" "+str(cow2-TempCounter2) + " " + str(cow3-TempCounter3)+ "\n")
-  else:
-    for j in range(ranges[i][0],ranges[i][1]+1):
-      if allCows[j]==1:
-        TempCounter1+=1
-      elif allCows[j]==2:
-        TempCounter2+=1
-      elif allCows[j]==3:
-        TempCounter3+=1
-    outFile.write(str(TempCounter1)+" " +str(TempCounter2)+ " " + str(TempCounter3) + "\n")
+#print(allQuer)
 
-outFile.close()
+
+
+for i in range(numberRanges):
+  holsteins=newArr[allQuer[i][1]][0]-newArr[allQuer[i][0]-1][0]
+  guerneys=newArr[allQuer[i][1]][1]-newArr[allQuer[i][0]-1][1]
+  jerseys=newArr[allQuer[i][1]][2]-newArr[allQuer[i][0]-1][2]
+  print(str(holsteins)+" "+str(guerneys)+" "+str(jerseys))
